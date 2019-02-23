3981e6021d08e5aeb7c20a9d6f850e633782fb52
Cristina Murillo <cristinamurillo@Cristinas-Air.home>
2019-02-23 17:12:22 -0500

    added pin component for marking map

--- src/Components/Mapbox.js
+++ src/Components/Mapbox.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
-import ReactMapGL, {NavigationControl} from 'react-map-gl';
-
+import ReactMapGL, {Marker, Popup, NavigationControl} from 'react-map-gl';
+import mapIcon from '../map-marker-icon.png'
+import Pin from './Pin'
 
 class Mapbox extends Component {
     state = {
         viewport: {
-          width: '90wh',
+          width: '130vh',
           height: '70vh',
           latitude: 40.730610,
           longitude: -73.935242,
           zoom: 10
-        }
+        },
+        popupInfo: null
       };
 
     updateViewport = (viewport) => {
         this.setState({viewport})
     }
+
+    renderOpportunityMarker(){
+        return(
+            <Marker
+                longitude={-73.935242}
+                latitude={40.730610} 
+            >
+            <Pin size={25}/>
+            </Marker>
+        )
+    }
+
+
+
       render() {
         return (
             <div className="map-container">
-          <ReactMapGL
-            {...this.state.viewport}
-            onViewportChange={this.updateViewport}
-            mapboxApiAccessToken={'pk.eyJ1IjoieHRpbmFtdXJpbGxvIiwiYSI6ImNqbzhxMTdyMjE5ZTUzcHFxYThuYnR2YWMifQ.j0zHHpzStnhBejvcPq0UXA'}
-            mapStyle={'mapbox://styles/mapbox/streets-v9'}
-          >
-            <div style={{position: 'absolute', right: 0}}>
-            <NavigationControl onViewportChange={this.updateViewport} />
-            </div>
-        </ReactMapGL>
- 
+                <ReactMapGL
+                    {...this.state.viewport}
+                    onViewportChange={this.updateViewport}
+                    mapboxApiAccessToken={'pk.eyJ1IjoieHRpbmFtdXJpbGxvIiwiYSI6ImNqbzhxMTdyMjE5ZTUzcHFxYThuYnR2YWMifQ.j0zHHpzStnhBejvcPq0UXA'}
+                    mapStyle={'mapbox://styles/mapbox/streets-v9'}
+                >
+                    {this.renderOpportunityMarker()}
+                    <div style={{position: 'absolute', right: 4, top: 4}}>
+                        <NavigationControl onViewportChange={this.updateViewport} />
+                    </div>
+                </ReactMapGL>
           </div>
         );
       }
