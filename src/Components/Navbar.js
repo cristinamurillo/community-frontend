e08f1f67105a615ecd70c3e3de261a6627d1ef4e
Cristina Murillo <cristinamurillo@Cristinas-Air.home>
2019-02-26 10:02:01 -0500

    if user is an organizer, icon displays in nav bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,14 +5,16 @@ import profileIcon from '../profile-icon.png'
 
 
 class Navbar extends Component {
+
+ 
     render() {
         return (
             <div className="nav">
                 <h5 className="title-logo">Acts of Democracy</h5>
-                <div className="icon-circle"><img src={menuIcon} className="icon" alt="menu-icon"/></div>
-                <img src={searchIcon} className="icon" alt="search-icon"/>
-                <img src={profileIcon} className="icon" alt="profile-icon"/>
-
+                <div className="icon-circle"><img src={menuIcon} className="icon" alt="Menu icon"/></div>
+                <img src={searchIcon} className="icon" alt="Search icon"/>
+                <img src={profileIcon} className="icon" alt="Profile icon"/>
+                {this.props.organization && <img src={this.props.organization.icon_url} className="icon" alt="Organization icon"/>}
             </div>
         );
     }
