df569f07105e9454d2f4b70d72a1f0dd8af6b23f
Cristina Murillo <murillocam96@gmail.com>
2019-03-11 15:22:50 -0400

    began to populate popup with event info

--- src/Components/EventInfo.js
+++ src/Components/EventInfo.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class EventInfo extends Component {
     render() {
         const {opportunity} = this.props
+        console.log(opportunity)
         return (
             <div>
-                <p>{opportunity.title}</p>
+                <h6 className= "popup-orgname">{opportunity.title}</h6>
+                <p className= "popup-orgname">Organization Name</p>
+                <img className="popup-img" src={opportunity['img-url']} alt="Event Image"/>
             </div>
         );
     }
